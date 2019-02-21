4d5d90d3ad06b451aa0715a7b3117eab7bbe4ce6
afarmelo <afarmelo@asu.edu>
2019-02-21 10:00:50 -0500

    Clean variables, formatting, logic

--- Deliverable_4/src/java/TripleCrownDatabase.java
+++ Deliverable_4/src/java/TripleCrownDatabase.java
@@ -23,93 +23,81 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class TripleCrownDatabase {
-   
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost:3306/new_schema";
-
-   static final String USER = "root";
-   static final String PASS = "Millie6667!";
-   
+    
    public static void main(String[] args) {
-   Connection connect = null;
-   Statement statemnt = null;
-   PreparedStatement statement=null;
-   try{
       
-      Class.forName(args[3]);
-
-      
-      System.out.println("Connecting to database...");
-      connect = DriverManager.getConnection(args[0],args[1],args[2]);
-
-      
-      System.out.println("Creating statement...");
-      statemnt = connect.createStatement();
-      
-      
-      
-      if(args[4].equals("query1")){
-      String query1;
-      query1 = "SELECT cashvaluegiven FROM prize WHERE cashvaluegiven >= 5000";
-      ResultSet rs = statemnt.executeQuery(query1);
-      
-       while(rs.next()){
-         
-         int cash = rs.getInt("cashvaluegiven");
+       Connection connect = null;
+       Statement stmnt = null;
+       PreparedStatement pStmnt=null;
+       ResultSet rs = null;
+   
+       if (args.length < 5) {
+	   System.out.print("USAGE: java ser322.TripleCrownDatabase");
+	   System.out.println(" <url> <user> <pass> <driver> <method>");
+	   System.exit(0);
+       }
        
-         System.out.println("cash value given: " + cash);
+       String url = args[0];
+       String user = args[1];
+       String pass = args[2];
+       String driver = args[3];
+       String method = args[4];
+            
+       try{      
+	   Class.forName(driver);
+      
+	   System.out.println("Connecting to database...");
+	   connect = DriverManager.getConnection(url, user, pass);
+      
+	   System.out.println("Creating statement...");
+	   stmnt = connect.createStatement();      
+	   
+	   if(method.equals("query1")){
+	       String query1;
+	       query1 = "SELECT cashvaluegiven FROM prize WHERE cashvaluegiven >= 5000";
+	       rs = stmnt.executeQuery(query1);
+      
+	       while(rs.next()){         
+		   int cash = rs.getInt("cashvaluegiven");       
+		   System.out.println("cash value given: " + cash);
+	       }
+	   } else if (method.equals("query2")){	       
+	       pStmnt=connect.prepareStatement("SELECT _name FROM trainer WHERE trainerID=?");
+	       pStmnt.setString(1, args[5]);
+	       rs = pStmnt.executeQuery();
+      
+	       while(rs.next()){
+		   String name=rs.getString("_name");
+		   System.out.println("NAME: " + name);
+	       }
+	   } else if (method.equals("insert")){
+	       pStmnt=connect.prepareStatement("INSERT INTO record VALUES (?,?,?)");
+	       pStmnt.setString(1,args[5]);
+	       pStmnt.setString(2,args[6]);
+	       pStmnt.setString(3, args[7]);
+	       pStmnt.executeUpdate();
+     
+	       System.out.println("success");
+	   }
+       } catch(Exception e) {
+	   System.out.println("Exception executing statement on database");
+	   e.printStackTrace();
+       } finally {
 
-      }
-     
-      rs.close();
-      }
-      
-      
-      if(args[4].equals("query2")){
-      statement=connect.prepareStatement("SELECT _name FROM trainer WHERE trainerID=?");
-      statement.setString(1, args[5]);
-      ResultSet rs2 = statement.executeQuery();
-      
-      while(rs2.next()){
-    	  String name=rs2.getString("_name");
-    	  System.out.println("NAME: " + name);
-      }
-      rs2.close();
-      
-      }
-
-      if(args[4].equals("insert")){
-    	  statement=connect.prepareStatement("INSERT INTO record VALUES (?,?,?)");
-    	  statement.setString(1,args[5]);
-    	  statement.setString(2,args[6]);
-    	  statement.setString(3, args[7]);
-    	  statement.executeUpdate();
-     
-    	  System.out.println("success");
-      }
-     
-      statemnt.close();
-      connect.close();
-   }catch(SQLException se){
-      
-      se.printStackTrace();
-   }catch(Exception e){
-      
-      e.printStackTrace();
-   }finally{
-      
-      try{
-         if(statemnt!=null)
-            statemnt.close();
-      }catch(SQLException se2){
-      }
-      try{
-         if(connect!=null)
-            connect.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }
+	   try {
+	       if (rs != null)
+		   rs.close();
+	       if (stmnt != null)
+		   stmnt.close();
+	       if (pStmnt != null)
+		   pStmnt.close();
+	       if (connect != null)
+		   connect.close();
+	   } catch(SQLException se){
+	       System.out.println("Exception closing resources");
+	       se.printStackTrace();
+	   }
+       }
+       System.out.println("disconnected..goodbye");
    }
-   System.out.println("disconnected..goodbye");
-}
 }
