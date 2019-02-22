e11b4a282fb9c3f877e080e3269fdfd3a21ba72e
afarmelo <afarmelo@asu.edu>
2019-02-22 08:06:00 -0500

    Remove unneccessary imports

--- Deliverable_4/src/java/TripleCrownDatabase.java
+++ Deliverable_4/src/java/TripleCrownDatabase.java
@@ -14,17 +14,6 @@ import java.sql.Statement;
 
 import java.util.Scanner;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 public class TripleCrownDatabase {
 
     private static String user;
